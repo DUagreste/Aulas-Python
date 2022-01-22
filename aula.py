69c7ba736aeec7b75c8359a2afde95ee394b58d0
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-01-22 18:31:19 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   aula.py
    
     Untracked files:
    	"Fun\303\247\303\265es.txt"

--- aula.py
+++ aula.py
@@ -1,3 +1,34 @@
-nome = "chicago"
+# Aprendendo listas
 
-print(nome[0])
+lista1 = [2.0, 3.5, 7.5, -2, 12]
+print(lista1)
+print(len(lista1))
+print(sum(lista1))
+print(max(lista1))
+print(min(lista1))
+
+nome = "Vitor"
+valor = range(10)
+
+lista2 = list(range(10))
+print(lista2)
+
+lista3 = list(nome)
+print(lista3)
+
+elemento = 8
+
+if elemento in lista2:
+    print("Este elemento está dentro da lista!")
+
+print(type(lista3))
+
+
+lista4 = ["gato", "cachorro", "peixe", "leão", "tigre"]
+print(lista4)
+lista4[0] = "ave"
+print(lista4)
+lista4[1:4] = ["girafa", "morcego", "elefante"]
+print(lista4)
+lista4[4:5] = ["aranha", "cobra"]
+print(lista4)
