d88aec00f0518ce2962022c5513e6642d48f0848
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-01-24 22:44:05 -0300

    Atualização dos estudos sobre Coleção:Tuplas

---
+++ Tuplas.py
@@ -0,0 +1,15 @@
+tupla = ("item1", "item2", "item3", "item1", "item1")
+print(tupla)
+print(len(tupla))
+print(type(tupla))
+
+print(tupla.count("item3"))
+
+uf = ("RN", "PE", "CE", "PB", "AL", "BA")
+print(type(uf))
+
+# Tuplas são mais utilizados em coisas que não devem ser mudadas,
+# como os estados brasileiros (exemplo acima).
+
+# É um coleção bem parecida com Listas, porém, são imutáveis.
+# Isto é, não há muitas funções, como append, insert, pop etc.
